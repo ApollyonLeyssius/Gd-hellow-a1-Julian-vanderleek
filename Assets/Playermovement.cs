e0bc0c753766ce4348f12ffe2db9d58a79c75d1d
ApollyonLeyssius <38302@ma-web.nl>
2025-01-27 11:41:49 +0100

    Test

--- Assets/Playermovement.cs
+++ Assets/Playermovement.cs
@@ -2,35 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playermovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f; // Normale loopsnelheid
+    public float sprintSpeed = 10f; // Sprint snelheid
+    public float jumpForce = 5f; // Kracht voor springen
+    private Rigidbody rb;
+    private bool isGrounded;
+    private Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>(); // Animator ophalen
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.S))
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+            Jump();
         }
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    private void Move()
+    {
+        // Beweging via toetsenbord (WASD / pijltjestoetsen)
+        float moveX = Input.GetAxis("Horizontal"); // Links/rechts (A/D of pijltjestoetsen)
+        float moveZ = Input.GetAxis("Vertical");   // Vooruit/achteruit (W/S of pijltjestoetsen)
+
+        // Bepaal de snelheid op basis van de sprinttoets
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
+
+        // Beweeg de speler
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        rb.MovePosition(rb.position + move * currentSpeed * Time.deltaTime);
+
+        // Set de animaties afhankelijk van de beweging
+        if (animator != null)
         {
-            transform.position -= new Vector3(1, 0, 0) * Time.deltaTime;
+            bool isMoving = moveX != 0 || moveZ != 0;
+            animator.SetBool("isMoving", isMoving); // Zet animatie voor bewegen
+            animator.SetFloat("Speed", isMoving ? currentSpeed : 0); // Zet snelheid voor animatie
+
+            // Zet de animatie voor springen
+            animator.SetBool("isJumping", !isGrounded);
         }
+    }
 
-        if (Input.GetKey(KeyCode.D))
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+            isGrounded = true;
         }
+    }
 
-        if (Input.GetKey(KeyCode.A))
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            transform.position -= new Vector3(0, 0, 1) * Time.deltaTime;
+            isGrounded = false;
         }
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Mijn game speelt zich af in een wereld met maar 1 stad waar in je rond kan rijde
 # Ik heb alle gebouwen neer gezeten en collision boxes neer gezet. 
 # Ik heb een plein gemaakt met bomen en anders plein dingen.
 # Ik heb alles in het plein collision gegeven.
+
+# Ik heb via de animator animaties toegevoegt aan mijn speler.
+# Ik heb via mijn beweeg script springen toegevoegt en vervolgens een animatie daarvoor
+# Ik heb munten toegevoegt en gelinkt met een script en een text element waardoor je ze kan oppakken en er een counter om hoog gaat daardoor
